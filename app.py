b0af8f14c511c5c6f32a285b6e3d2dfdac4e8bd6
chrisjcoder <chrisj_j@hotmail.com>
2019-08-15 17:50:10 -0500

    cleanup code-remove spaces

--- app.py
+++ app.py
@@ -8,16 +8,12 @@ import pymongo
 # Create an instance of our Flask app
 app = Flask(__name__)
 
-# # Create connection variable
+# # Create connection variable- (retaining this section for reference)
 # conn = 'mongodb://localhost:27017'
-
 # # Pass connection to the pymongo instance.
 # client = pymongo.MongoClient(conn)
-
 # # Connect to a database. Will create one if not already available.
 # db = client.mars_db
-
-
 # # Drops collection if available to remove duplicates
 # db.mars_data.drop()
 
